1fefb565f164dd15e77e07eb3f470f805f2dd595
Lars <lars@roth-kl.de>
2025-09-29 08:40:25 +0200

    chore: :bookmark: Release 1.7.1 (#19)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="photo_organizer",
-    version="1.7.0",
+    version="1.7.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [

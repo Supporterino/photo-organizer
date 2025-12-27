d46b6075613911b3b7fb7212dc56974051579fed
Supporterino <lars@roth-kl.de>
2025-12-27 17:07:40 +0100

    chore: :bookmark: Release 1.7.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="photo_organizer",
-    version="1.7.1",
+    version="1.7.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
